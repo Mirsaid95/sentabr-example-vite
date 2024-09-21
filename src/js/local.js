919cbbbf833b7d8c8abca9ad2ae82012f13b585e
Mirsaid Akbaraliyev <akbaraliyev522@mail.ru>
2024-09-22 03:46:07 +0500

    initial commit

---
+++ src/js/local.js
@@ -0,0 +1,12 @@
+const baseUrl = "https://fakestoreapi.com/products";
+
+export async function getData(path) {
+    try {
+        const res = await fetch(`${baseUrl}/${path}`);
+        const data = await res.json()
+        return data;
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import {getData} from './local.js';
+
+getData('1').then((data) => {
+    console.log(data);
+});
